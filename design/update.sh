bbc64514062abbf010eb0b7399624a7ed9a0c032
Yo'av Moshe <bjesus@gmail.com>
2010-06-15 18:29:30 +0300

    update script

--- design/update.sh
+++ design/update.sh
@@ -1,3 +1,6 @@
 haml index.html.haml > index.html
 haml item1.html.haml > item1.html
+haml welcome.html.haml > welcome.html
+haml register.html.haml > register.html
+haml upload.html.haml > upload.html
 sass css/style.sass > css/style.css
