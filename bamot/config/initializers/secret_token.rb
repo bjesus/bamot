148fb748b9eea31267c38e91dbcf8d811d7ce79f
Yo'av Moshe <bjesus@gmail.com>
2010-09-16 13:48:00 +0200

    rails3

---
+++ bamot/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Bamot::Application.config.secret_token = '9796a69712a9d684a2f58c15ef585af04b312fa5c47fb1572e61009d5d5f0fc204af722edc4fc15248ffb9008f3d03034704eaf66dd2a93ced8b5178918cdecf'
