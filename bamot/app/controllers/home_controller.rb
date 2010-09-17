ef1d96513cd37d8f769a370eb07a82eade05cc28
Yo'av Moshe <bjesus@gmail.com>
2010-09-17 14:11:05 +0200

    front page new shows real photos and news

--- bamot/app/controllers/home_controller.rb
+++ bamot/app/controllers/home_controller.rb
@@ -4,8 +4,10 @@ class HomeController < ApplicationController
     @creations = Creation.order("created_at DESC").limit(5)
     @users = User.order("created_at DESC").limit(5)
     @news = Creation.where('news'=>true).order("created_at DESC").limit(2)
-    ids = Creation.find( :all, :select => 'id' ).map( &:id )
-    @gallery = Creation.find( (1..4).map { ids.delete_at( ids.size * rand ) } )
+    pids = Creation.where('image_file_name', 'topic_id'=>true).find( :all, :select => 'id' ).map( &:id )
+    @photosgallery = Creation.find( (1..2).map { pids.delete_at( pids.size * rand ) } )
+    tids = Creation.where('image_file_name'=>nil, 'topic_id'=>true).find( :all, :select => 'id' ).map( &:id )
+    @textgallery = Creation.find( (1..2).map { tids.delete_at( tids.size * rand ) } )
   end
 
 end
