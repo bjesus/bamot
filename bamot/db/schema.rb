35a9537408c20dbedc1f6ff8063760b5a0cfdd39
Yo'av Moshe <bjesus@gmail.com>
2010-09-17 14:10:33 +0200

    scheme updated with image by paperclip

--- bamot/db/schema.rb
+++ bamot/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20100916210335) do
+ActiveRecord::Schema.define(:version => 20100917102644) do
 
   create_table "creations", :force => true do |t|
     t.string   "name"
@@ -22,6 +22,10 @@ ActiveRecord::Schema.define(:version => 20100916210335) do
     t.text     "content"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "image_file_name"
+    t.string   "image_content_type"
+    t.integer  "image_file_size"
+    t.datetime "image_updated_at"
   end
 
   create_table "extfiles", :force => true do |t|
