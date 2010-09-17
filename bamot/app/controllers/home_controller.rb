31691b457bfa804533f0f9b387048eef4f1d9cbb
Yo'av Moshe <bjesus@gmail.com>
2010-09-17 19:21:33 +0200

    front page

--- bamot/app/controllers/home_controller.rb
+++ bamot/app/controllers/home_controller.rb
@@ -8,6 +8,10 @@ class HomeController < ApplicationController
     @photosgallery = Creation.find( (1..2).map { pids.delete_at( pids.size * rand ) } )
     tids = Creation.where('image_file_name'=>nil, 'topic_id'=>true).find( :all, :select => 'id' ).map( &:id )
     @textgallery = Creation.find( (1..2).map { tids.delete_at( tids.size * rand ) } )
+    @topic = Topic.first(:conditions => 'start>'+'2010-09-17')
+    @kinds = Kind.all
+    rids = Creation.find( :all, :select => 'id' ).map( &:id )
+    @random = Creation.find( (1..5).map { rids.delete_at( rids.size * rand ) } )
   end
 
 end
