950643acca4e690819e4f5bc2db7d1d8e5174dfa
Yo'av Moshe <bjesus@gmail.com>
2010-09-19 17:37:52 +0200

    link to "all users"

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -5,7 +5,7 @@ class HomeController < ApplicationController
   def index
     @user = current_user
     @creations = Creation.order("created_at DESC").limit(5)
-    @users = User.order("created_at DESC").limit(5)
+    @users = User.order("created_at DESC").limit(4)
     @news = Creation.where('news'=>true).order("created_at DESC").limit(2)
     pids = Creation.where('image_file_name', 'topic_id'=>true).find( :all, :select => 'id' ).map( &:id )
     @photosgallery = Creation.find( (1..2).map { pids.delete_at( pids.size * rand ) } )
