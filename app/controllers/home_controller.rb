9274044ec2a12ad95eedda8e59600cfdb727432b
bjesus <bjesus@ganuv.(none)>
2011-01-01 12:11:08 +0200

    now it's displaying the correct images

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -7,7 +7,7 @@ class HomeController < ApplicationController
     @creations = Creation.order("created_at DESC").limit(5)
     @users = User.order("created_at DESC").limit(4)
     @news = Creation.where('news'=>true).order("created_at DESC").limit(2)
-    pids = Creation.where('image_file_name', 'topic_id'=>true).find( :all, :select => 'id' ).map( &:id )
+    pids = Creation.where(:kind_id => '2', :topic_id=>true).find( :all, :select => 'id' ).map( &:id )
     @photosgallery = Creation.find( (1..2).map { pids.delete_at( pids.size * rand ) } )
     tids = Creation.where('image_file_name'=>nil, 'topic_id'=>true).find( :all, :select => 'id' ).map( &:id )
     @textgallery = Creation.find( (1..2).map { tids.delete_at( tids.size * rand ) } )
