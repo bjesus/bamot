bf76d8d025c1280fc0957dc2580f62c9b0208906
Yo'av Moshe <bjesus@gmail.com>
2011-05-18 15:17:02 +0300

    env

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -11,7 +11,7 @@ Bamot::Application.configure do
 
   # Specifies the header that your server uses for sending files
   config.action_dispatch.x_sendfile_header = "X-Sendfile"
-
+  config.action_mailer.default_url_options = { :host => 'davar.drorisrael.org.il' }
   # For nginx:
   # config.action_dispatch.x_sendfile_header = 'X-Accel-Redirect'
 
